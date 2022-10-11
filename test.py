dc354f0af739ec90f8362e854ada5bfb5bff7b88
ElementWizard <gerloffroman@gmail.com>
2022-10-12 00:58:47 +0200

    Updated repo to Games  added "Mensch-ärgere-dich-nicht"

--- test.py
+++
@@ -1,8 +0,0 @@
-from main import basics_decide_soft_total
-
-for b in range(2, 10):
-    for c in range(2, 12):
-        player = [11, b]
-        dealer = c
-
-        print(player, dealer, basics_decide_soft_total(player, dealer))
